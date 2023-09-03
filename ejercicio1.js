e0e347ca1abf5c831a1b5bdfb9cf1550909ba98a
Gianluca Pettenon <115878175+Giany74@users.noreply.github.com>
2023-09-03 21:34:38 +0200

    Add files via upload
    first exercise

---
+++ ejercicio1.js
@@ -0,0 +1,23 @@
+/*
+Crea un archivo ejercicio1.js que tenga un objeto con los siguientes campos: 
+Nombre, 
+apellidos, 
+un array con los temas del bootcamp que ya conoces, 
+si estás en busqueda activa con un boolean 
+y un array de objetos el cual tenga un link a alguna red social con el nombre y link de la red social. 
+(Con uno vale, Github por ejemplo, pero dentro de un array).
+*/
+
+const user_object = {
+    Nombre : 'Gianluca',
+    Apellidos: 'Pettenon',
+    themeBootcamp: ['Git-GitHub', 'html-css', 'JavaScript'],
+    ActiveSearch: true,
+    socialLinks: {
+        github: 'https://github.com/Giany74',
+        linkedin: 'https://www.linkedin.com/in/gianluca-pettenon-a4051a8a/',
+        instagram: 'https://www.instagram.com/gianluca.pettenon/',
+        behance: 'https://www.behance.net/gianlucapettenon',
+        facebook: 'https://www.facebook.com/gianluca.pettenon/'
+    }
+};
